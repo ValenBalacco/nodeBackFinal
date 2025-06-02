2dca5786ca3e1d3b867aae9e515c386455aadef6
ValenBalacco <valentino.balacco.16@gmail.com>
2025-06-02 08:38:03 -0300

    .

--- src/routes/ordenCompraRoutes.js
+++ src/routes/ordenCompraRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const ordenCompraController = require('../controllers/ordenCompraController');
-const { authenticateToken, authorizeAdmin } = require('../Midleware/auth'); // Agrega authorizeAdmin aquí
+const { authenticateToken, authorizeAdmin } = require('../Midleware/auth'); 
 
 router.get('/', authenticateToken, authorizeAdmin, ordenCompraController.getAll);
 router.get('/:id', authenticateToken, authorizeAdmin, ordenCompraController.getById);
